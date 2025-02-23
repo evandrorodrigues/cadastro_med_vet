21afce7ea5020139e3fcf44296dbf06e068c1937
Evandro Felipe Rodrigues Lima De Carvalho <30915993+evandrorodrigues@users.noreply.github.com>
2025-02-23 17:42:29 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EscalaMedicos from './EscalaMedicos';
+
+function App() {
+  return (
+    <div className="App">
+      <EscalaMedicos />
+    </div>
+  );
+}
+
+export default App;
